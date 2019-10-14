a148433115b85153149cfc259a307acc27c2cd6e
MarcoSantonastasi <marco.santonastasi@altesis-pro.com>
2019-10-14 17:05:21 +0200

    Iteration 4 completed

--- src/components/DimensionConfigurator.tsx
+++ src/components/DimensionConfigurator.tsx
@@ -5,9 +5,14 @@ export enum Dimension {
   height = "height"
 }
 
+interface IonChangeCallback {
+  (configuratorName: string, lengthValue: number): void;
+}
+
 interface DimConfProps {
   dimension: Dimension;
   length: number;
+  onChangeCallback: IonChangeCallback;
 }
 
 interface DimConfState {}
@@ -18,11 +23,33 @@ export default class DimensionConfigurator extends Component<
 > {
   render() {
     return (
-      <div style={{ width: `${this.props.length}px`, backgroundColor: "blue" }}>
-        <p>
-          I'm a {Dimension[this.props.dimension]} configurator of length{" "}
-          {this.props.length}
-        </p>
+      <div>
+        <label htmlFor="setLength">Set the leng of the div </label>
+        <input
+          type="number"
+          name="setLength"
+          id="setLength"
+          min="0"
+          max="500"
+          step="25"
+          value={this.props.length}
+          onChange={e =>
+            this.props.onChangeCallback(
+              this.props.dimension,
+              parseInt(e.target.value)
+            )
+          }
+        />
+        <br />
+
+        <div
+          style={{ width: `${this.props.length}px`, backgroundColor: "blue" }}
+        >
+          <p>
+            I'm a {Dimension[this.props.dimension]} configurator of length{" "}
+            {this.props.length}
+          </p>{" "}
+        </div>
       </div>
     );
   }
--- src/components/SquareConfigurator.tsx
+++ src/components/SquareConfigurator.tsx
@@ -1,13 +1,48 @@
 import React, { Component } from "react";
 import DimensionConfigurator, { Dimension } from "./DimensionConfigurator";
+import { string } from "prop-types";
 
-export default class SquareConfigurator extends Component {
+interface SqConfProps {}
+
+interface SqConfState {
+  width: number;
+  height: number;
+  [configuratorName: string]: number;
+}
+
+export default class SquareConfigurator extends Component<
+  SqConfProps,
+  SqConfState
+> {
+  state = { height: 250, width: 250 };
+
+  handleOnChange(configuratorName: string, lengthValue: number): void {
+    this.setState({ [configuratorName]: lengthValue });
+  }
+
   render() {
     return (
       <div>
         <p>Hello I'm a square configurator</p>
-        <DimensionConfigurator dimension={Dimension.width} length={350} />
-        <DimensionConfigurator dimension={Dimension.height} length={490} />
+        <DimensionConfigurator
+          dimension={Dimension.width}
+          length={this.state.width}
+          onChangeCallback={this.handleOnChange.bind(this)}
+        />
+        <DimensionConfigurator
+          dimension={Dimension.height}
+          length={this.state.height}
+          onChangeCallback={this.handleOnChange.bind(this)}
+        />
+        <div
+          style={{
+            height: `${this.state.height}px`,
+            width: `${this.state.width}px`,
+            backgroundColor: "red"
+          }}
+        >
+          This is a square div
+        </div>
       </div>
     );
   }
